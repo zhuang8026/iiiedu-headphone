2d1b839a9936504df579c547e2dc1b8d20f09bb0
william_8026 <zhuang8026@gmail.com>
2020-06-12 10:24:02 +0800

    how to use swiper ?

--- client/src/components/HomeSlider/config.js
+++ client/src/components/HomeSlider/config.js
@@ -3,16 +3,25 @@ export const SwiperImg = [
     {
         linkUrl: 'https://treefonts.com/',
         picUrl: require('../../assets/img/bg_1.jpg'),
-        name: 'background image'
+        name: 'background image',
+        h1text: 'NEW HEADPHONES',
+        ptext: 'A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by OTIS.',
+        btnText: 'DISCOVER'
     },
     {
         linkUrl: 'https://treefonts.com/',
         picUrl: require('../../assets/img/bg_2.jpg'),
-        name: 'background image'
+        name: 'background image',
+        h1text: 'ALL DREAMLIKE',
+        ptext: 'A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by OTIS.',
+        btnText: 'DISCOVER'
     },
     {
         linkUrl: 'https://treefonts.com/',
         picUrl: require('../../assets/img/bg_3.jpg'),
-        name: 'background image'
+        name: 'background image',
+        h1text: 'NEW HEADPHONES',
+        ptext: 'A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by OTIS.',
+        btnText: 'DISCOVER'
     }
 ];
