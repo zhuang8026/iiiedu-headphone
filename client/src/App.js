da59e86fa542bbe4402b09fb232bee1824fa5844
Alice <alicehpai@gmail.com>
2020-06-13 18:04:06 +0800

    環境建置

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,30 @@
-import React ,{ Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect  } from "react-router-dom"
+import React, { Fragment, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
 
-import MyNavBar from './components/Navbar';
-import MyMenu from './components/NavbarMenu';
-import MyFooter from './components/Footer';
-
+import MyNavBar from './components/Navbar'
+import MyMenu from './components/NavbarMenu'
+import MyFooter from './components/Footer'
+//賣家功能
+import Seller_center from './pages/alice_seller/Seller_center'
 
 function App() {
-  useEffect(()=>{
-    let shop_btn = document.getElementById('shopping');
-    let side_menu = document.getElementsByClassName('header-side-menu')[0]; 
-    let page_cover = document.getElementsByClassName('nav-page-cover')[0]; 
-    let menu_close = document.getElementsByClassName('side-menu-close')[0]; 
-    shop_btn.addEventListener('click',()=>{
-        side_menu.classList.add('header-side-menu-active');
-        page_cover.classList.add('nav-page-cover-active');
-        
-    });
-    page_cover.addEventListener('click',()=>{
-        side_menu.classList.remove('header-side-menu-active');
-        page_cover.classList.remove('nav-page-cover-active');
-        
-    });
-    menu_close.addEventListener('click',()=>{
-        side_menu.classList.remove('header-side-menu-active');
-        page_cover.classList.remove('nav-page-cover-active');
-        
-    });
-  },[])
   return (
     <Router>
-        <Fragment>
-            <header>
-              <MyNavBar/>
-              <MyMenu/>
-            </header>
-            <MyFooter/>
-        </Fragment>
+      <>
+        <Switch>
+        {/* 分頁切換router */}
+          <Route path="/seller-center">
+            <Seller_center />
+          </Route>
+        </Switch>
+      </>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Alice_seller/Left_nav.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Left_nav extends Component {
+    render() {
+        return (
+            <div>
+                1111111111111
+            </div>
+        )
+    }
+}
+
+export default Left_nav
