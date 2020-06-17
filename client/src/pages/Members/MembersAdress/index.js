d69ba7a6f6a3ae38e19432e4d9431e0562b57a15
william_8026 <zhuang8026@gmail.com>
2020-06-18 00:05:41 +0800

    icon add to 110 price

--- client/src/pages/Members/MembersAdress/index.js
+++ client/src/pages/Members/MembersAdress/index.js
@@ -28,7 +28,34 @@ function MembersAdress() {
                             </div>
                             {/* 主要內容 */}
                             <div className="members_address_r_bottom">
-
+                                <div className="r_bottom_left">
+                                    <ul>
+                                        <li>
+                                            <div className="r_bottom_nodel">
+                                                <label htmlFor="use">姓名</label>
+                                                <span className="iconfont icon-user_2"></span>
+                                                <input id="use" className="mem_input" placeholder="莊杰翰" value={'莊杰翰'}/>
+                                            </div>
+                                            <span className="r_bottom_err">賬號不可修改</span>
+                                        </li>
+                                        <li>
+                                            <div className="r_bottom_del">
+                                                <label htmlFor="name">手機</label>
+                                                <span className="iconfont icon-user_2"></span>
+                                                <input id="name" className="mem_input" placeholder="0988220903"/>
+                                            </div>
+                                            <span className="r_bottom_err">姓名不符合格式</span>
+                                        </li>
+                                        <li>
+                                            <div className="r_bottom_del">
+                                                <label htmlFor="email">地址</label>
+                                                <span className="iconfont icon-user_2"></span>
+                                                <input id="email" className="mem_input" placeholder="新北市火星區地球路一段413號１樓 全家火星噴火店"/>
+                                            </div>
+                                            <span className="r_bottom_err">email格式做錯</span>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
