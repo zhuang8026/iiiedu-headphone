3059fa802c6eaa37582d0561bd2bc31bfbb88a8f
yiyang0903 <64294452+yiyang0903@users.noreply.github.com>
2020-06-18 21:10:06 +0800

    很醜的葉碼改掉了  細節葉快做完了

--- client/src/pages/Product/ProductDetail/ProductDetailRight/index.js
+++ client/src/pages/Product/ProductDetail/ProductDetailRight/index.js
@@ -1,5 +1,5 @@
 // 函式元件
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import {
   BrowserRouter as Router,
   Route,
@@ -12,8 +12,10 @@ import {
 //antd
 import { Radio } from 'antd'
 // scss
-import '../../../../assets/scss/Product_Detail.scss'
+
+import '../../../../assets/scss/Product_DetailRight.scss'
 function ProductDetailRight() {
+  const [total, setTotal] = useState(0)
   return (
     <>
       <div className="YyDetailRight">
@@ -26,6 +28,39 @@ function ProductDetailRight() {
             提供您絕佳聆聽體驗。 Sennheiser Smart Control
             app應用程式中的提供EQ自訂功能， 同時也提供軟體更新和 podcast 模式。.
           </p>
+        </div>
+        <div className="Yybuttoninner">
+          <div className="pro_color">
+            <div className="pro_a"></div>
+            <div className="pro_b"></div>
+            <div className="pro_c"></div>
+          </div>
+          <div className="Yyinputgroup">
+            <div className="Yybtnout">
+              <button
+                className="Yybtn1"
+                onClick={() => {
+                  setTotal(total + 1)
+                }}
+              >
+                +
+              </button>
+              <span>{total}</span>
+              <button
+                className="Yybtn2"
+                onClick={() => {
+                  setTotal(total - 1)
+                }}
+              >
+                -
+              </button>
+            </div>
+            <div className="Yycart">
+              <button className="addto">加入比較</button>
+              <button className="addto">加入最愛</button>
+              <button className="addto">加入購物車</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
