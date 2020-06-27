ce3f256e1752d6d2c8c3f42d2b248712d4684d30
yiyang0903 <64294452+yiyang0903@users.noreply.github.com>
2020-06-28 03:43:51 +0800

    禮物卡+localStorage完成

---
+++ client/src/pages/Gift/GiftHeader/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect,useState } from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.css';
+import { withRouter, Link } from 'react-router-dom';
+import Aboutimg from '../../../assets/img/About/A01.jpg'
+import Aboutimg2 from '../../../assets/img/About/A02.jpg'
+import Aboutimg3 from '../../../assets/img/About/A07.jpg'
+//antd
+// import { message } from 'antd';
+
+function GiftHeader() {
+  const params = {
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+  },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+   
+  }
+
+  return (
+    <div className="WiHeader">
+      <Swiper {...params}>
+      <div><img src={Aboutimg}></img>
+      <h1>禮物卡</h1>
+      </div>
+       <div><img src={Aboutimg2}></img>
+       <h1>禮物卡</h1>
+       </div>
+       <div id="Aboutimg3"><img src={Aboutimg3}></img>
+       <h1>禮物卡</h1>
+       </div>
+  </Swiper>
+    </div>
+
+ 
+
+
+  )
+}
+export default withRouter(GiftHeader)
--- client/src/pages/Gift/index.js
+++ client/src/pages/Gift/index.js
@@ -1,14 +1,18 @@
 // 函式元件
 import React, { Fragment } from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom'
-
+import GiftHeader from './GiftHeader/index'
+import GiftAbout from './GiftAbout/index'
+import GiftBuy from './GiftBuy/index'
 // scss
 // import './_menu.scss'
 
 function WiGift(props) {
     return (
         <Fragment>
-        禮物卡
+        <GiftHeader />
+        <GiftAbout />
+        <GiftBuy />
         </Fragment>
     )
 
