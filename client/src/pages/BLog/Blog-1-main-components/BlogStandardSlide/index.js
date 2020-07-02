a0c2fdc4cbabdc1e4d0206563b506870237d93cd
kirin1977 <wangyungching1977@gmail.com>
2020-07-02 19:07:01 +0800

    輪播牆ok  待調整

--- client/src/pages/BLog/Blog-1-main-components/BlogStandardSlide/index.js
+++ client/src/pages/BLog/Blog-1-main-components/BlogStandardSlide/index.js
@@ -1,14 +1,19 @@
 // 函式元件
 import React, { Fragment, useEffect } from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-  Link,
-  NavLink,
-  withRouter,
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+    Link,
+    NavLink,
+    withRouter,
 } from 'react-router-dom'
+// Swiper
+import Swiper from 'react-id-swiper';
+// import SwiperAnimation from '@cycjimmy/swiper-animation';
+// import "animate.css/animate.min.css";
+// import testSlider from '../testSlider'
 
 // -------------------- components --------------------
 
@@ -18,69 +23,76 @@ import {
 // -------------------- imgs --------------------
 import slide from '../../../../assets/img/blog-img/blog-standard/slide.png'
 import top from '../../../../assets/img/blog-img/blog-standard/top.png'
+import { SwiperImg } from './config.js';
 
 // -------------------- func --------------------
 
 function BlogMainStandardSlide(props) {
-  const { userdata, setUserdata } = props
-  return (
-    <>
-      <div className="carouselWall-blog">
-        <div className="imageWall-blog">
-          <figure className="blog-standard-fig wallFig blog-cover">
-            <img src={top} alt="" />
-          </figure>
-        </div>
-        <div className="slideWall">
-          <div className="slides slider-container" id="sliderContainer">
-            <ul
-              className="list-unstyled slider-images blog-d-flex position-absolute"
-              id="sliderImages"
-            >
-              <li>
-                <img className="slide-images" src={slide} alt="" />
-              </li>
-              <li>
-                <img src={slide} alt="" />
-              </li>
-              <li>
-                <img src={slide} alt="" />
-              </li>
-              <li>
-                <img src={slide} alt="" />
-              </li>
-              <li>
-                <img src={slide} alt="" />
-              </li>
-            </ul>
-            <ul
-              className="list-unstyled position-absolute slider-dots blog-d-flex justify-content-center"
-              id="sliderDots"
-            >
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-            <Link to=""
-              role="button"
-              id="goNext"
-              className="dir-btn position-absolute dir-right"
-            >
-              <i className="fas fa-chevron-right"></i>
-            </Link>
-            <Link to=""
-              role="button"
-              id="goPrev"
-              className="dir-btn position-absolute dir-left"
-            >
-              <i className="fas fa-chevron-left"></i>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+    const { userdata, setUserdata } = props
+    // const swiperAnimation = new SwiperAnimation();
+    const mySwiper = {
+        slidesPerView: 3,
+        centeredSlides: true,
+        centeredSlidesBounds: true,
+        // spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    }
+
+
+
+    return (
+        <>
+            <div className="carouselWall-blog">
+                <div className="imageWall-blog">
+                    <figure className="blog-standard-fig wallFig blog-cover">
+                        <img src={top} alt="" />
+                    </figure>
+                </div>
+                <div className="slideWall">
+                    <div className="slides slider-container" id="sliderContainer">
+
+                        <div className="swiper-container">
+                            <div className="swiper-wrapper">
+                                <Swiper {...mySwiper}>
+                                    <div className="swiper-slide">
+                                        <figure className="s-f">
+                                            <img className="s-f-i blog-cover" src={slide} alt="" />
+                                        </figure>
+                                    </div>
+                                    <div className="swiper-slide">
+                                        <figure className="s-f">
+                                            <img className="s-f-i blog-cover" src={slide} alt="" />
+                                        </figure>
+                                    </div>
+                                    <div className="swiper-slide">
+                                        <figure className="s-f">
+                                            <img className="s-f-i blog-cover" src={slide} alt="" />
+                                        </figure>
+                                    </div>
+                                    <div className="swiper-slide">
+                                        <figure className="s-f">
+                                            <img className="s-f-i blog-cover" src={slide} alt="" />
+                                        </figure>
+                                    </div>
+                                </Swiper>
+                            </div>
+
+
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 export default withRouter(BlogMainStandardSlide)
