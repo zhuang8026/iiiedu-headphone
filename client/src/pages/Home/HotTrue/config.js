037a43650fc659c3d46b81293552d29513396ee1
william_8026 <zhuang8026@gmail.com>
2020-07-02 16:47:41 +0800

    首頁RWD

---
+++ client/src/pages/Home/HotTrue/config.js
@@ -0,0 +1,49 @@
+// 頭戴耳機 品牌
+export const SwiperTrue = [
+    {
+        piclinkUrl: '/*****',
+        picUrl: require('../../../assets/img/bg_6p_1.png'),
+        name: 'bRS2e',
+        p1text: '非常適合運動',
+        p2text: '電池續航時間長達 15 小時 *',
+        p3text: '運動傳感器',
+        p4text: '防汗防塵',
+    },
+    {
+        piclinkUrl: '/*****',
+        picUrl: require('../../../assets/img/bg_6p_1.png'),
+        name: 'bRS2e',
+        p1text: '非常適合運動',
+        p2text: '電池續航時間長達 15 小時 *',
+        p3text: '運動傳感器',
+        p4text: '防汗防塵',
+    },
+    {
+        piclinkUrl: '/*****',
+        picUrl: require('../../../assets/img/bg_6p_1.png'),
+        name: 'bRS2e',
+        p1text: '非常適合運動',
+        p2text: '電池續航時間長達 15 小時 *',
+        p3text: '運動傳感器',
+        p4text: '防汗防塵',
+    },
+    {
+        piclinkUrl: '/*****',
+        picUrl: require('../../../assets/img/bg_6p_1.png'),
+        name: 'bRS2e',
+        p1text: '非常適合運動',
+        p2text: '電池續航時間長達 15 小時 *',
+        p3text: '運動傳感器',
+        p4text: '防汗防塵',
+    },
+    {
+        piclinkUrl: '/*****',
+        picUrl: require('../../../assets/img/bg_6p_1.png'),
+        name: 'bRS2e',
+        p1text: '非常適合運動',
+        p2text: '電池續航時間長達 15 小時 *',
+        p3text: '運動傳感器',
+        p4text: '防汗防塵',
+    },
+    
+];
