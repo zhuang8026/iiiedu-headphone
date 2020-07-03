dcf24e6641b2465fdcdfe94eff7196b57959e256
grassyau <kj25940858@gmail.com>
2020-07-03 16:31:33 +0800

    TX

--- client/src/pages/Home/HotTrue/index.js
+++ client/src/pages/Home/HotTrue/index.js
@@ -10,7 +10,7 @@ import 'swiper/css/swiper.css';
 
 import {SwiperTrue} from './config.js';
 
-import a from '../../../assets/img/bg_6p_2.png'
+
 
 
 function HotTrue() {
@@ -88,7 +88,7 @@ function HotTrue() {
                                     <div className="true_items_card swiper-lazy">
                                         <h3>{ data.name }</h3>
                                         <a>
-                                            <img src={a} alt="商品列表"/>
+                                            <img src={data.picUrl} alt="商品列表"/>
                                         </a>
                                         <ul>
                                             <li><i className="iconfont icon-correct"></i>{ data.p1text }</li>
