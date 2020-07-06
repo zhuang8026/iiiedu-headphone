bcdae7bf42c2a9392ff5c488ebe4210464abc61d
yiyang0903 <64294452+yiyang0903@users.noreply.github.com>
2020-07-06 19:21:31 +0800

    地圖完成了

--- client/src/pages/Store/WistoreHeader/index.js
+++ client/src/pages/Store/WistoreHeader/index.js
@@ -7,6 +7,8 @@ import 'react-leaflet-markercluster/dist/styles.min.css'
 import L from 'leaflet'
 import picja from '../../../assets/img/japen.jpg'
 import pictw from '../../../assets/img/tw.jpg'
+import pickoa from '../../../assets/img/koa.jpg'
+import picusa from '../../../assets/img/usa.jpg'
 import {Place} from './config'
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -22,11 +24,12 @@ L.Icon.Default.mergeOptions({
 
 function WiMap() {
   const popupText = {
-    fontSize: "15px",
+    fontSize: "24px",
+    color:'midnightblue'
 
   };
   const Wih1 = {
-    fontSize: "36px",
+    fontSize: "34px",
 
   };
  
@@ -176,16 +179,49 @@ const CountrySelect = Place.map((data, index) =>
         <img src={picja}></img>
         <p></p>
         <h1 style={Wih1}>Otis東京本店</h1>
-        <p>做為發源地，我們的在挑高的格局表現了接受人群的氣派大方。屋頂、樓梯、
-        柱子或其他裝潢，風格都和西方建築很接近，中西混合體的柱頭更是最大的特色。<br></br>
+        <p>在日本造屋基本結構上添加許多西方元素，門廊與門廳以華麗的希臘立柱打造，繁複的花紋、勛章、彩帶，以泥塑工法雕琢，細節精緻，引人入勝。<br></br>
         <br></br>
         <h2>地址</h2>
-        台北市復興南路一段390號2樓
+        東京都港區白金台5-20-2
         </p>
         <h1 style={Wih1}>Otis大阪分店</h1>
         <p  className="wistorep">
         <h2>地址</h2>
-        台北市復興南路一段390號2樓
+        大阪市中央區難波5丁目1番5號
+        </p>
+      </div>
+      )}
+      {maporder === '3' && (
+      <div className="wiheaderna">
+        <img src={pickoa}></img>
+        <p></p>
+        <h1 style={Wih1}>Otis首爾本店</h1>
+        <p>在日本造屋基本結構上添加許多西方元素，門廊與門廳以華麗的希臘立柱打造，繁複的花紋、勛章、彩帶，以泥塑工法雕琢，細節精緻，引人入勝。<br></br>
+        <br></br>
+        <h2>地址</h2>
+        首爾特別市中區乙支路66
+        </p>
+        <h1 style={Wih1}>Otis釜山分店</h1>
+        <p  className="wistorep">
+        <h2>地址</h2>
+        釜山廣域市海雲臺區海雲臺海邊路264
+        </p>
+      </div>
+      )}
+      {maporder === '4' && (
+      <div className="wiheaderna">
+        <img src={picusa}></img>
+        <p></p>
+        <h1 style={Wih1}>Otis紐約本店</h1>
+        <p>雙重斜坡屋頂是其重要特色，通常使用魚鱗狀瓦片覆蓋，屋頂頂部邊緣有鐵制裝飾環繞，屋檐下有裝飾型的托架<br></br>
+        <br></br>
+        <h2>地址</h2>
+        1 E. 42nd Street New York , NY 10017 U.S.A.
+        </p>
+        <h1 style={Wih1}>Otis俄亥俄分店</h1>
+        <p  className="wistorep">
+        <h2>地址</h2>
+        he Ohio State University，1653 Enarson Hall ，154 W 12th Avenue， Columbus, Ohio 43210
         </p>
       </div>
       )}
