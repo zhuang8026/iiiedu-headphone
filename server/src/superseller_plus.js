3e2a857df3bccf44a1ad93154c8e44ab36c6925b
kirin1977 <wangyungching1977@gmail.com>
2020-07-07 17:16:49 +0800

    items和pic資料庫寫入 測試ok

--- server/src/superseller_plus.js
+++ server/src/superseller_plus.js
@@ -26,38 +26,51 @@ app.use(express.json());
 // app.use(cors());
 app.use('/avatar', express.static('uploads'));
 
-//================================================== root ==============================================================
-// http://localhost:3009/superseller_plus/
-router.get('/', (req, res) => {
-    res.send('superseller_plus root 測試ok');
-});
-
-//================================================== 新增 ==============================================================
-// (測試ok)
-// 新增產品
-// http://localhost:3009/superseller_plus/add
-router.post('/add', upload.none(), (req, res) => {
+// function
+// 新增商品
+const doAddItems = async (req) => {
+    let itemName = '111';
+    let itemImg = '111';
+    let colorid = 1;
+    let itemsbrand = '111';
+    let itemstype = '111';
+    let itemPrice = 1;
+    let itemQty = 1;
+    let itemsales = 1;
+    let itemsstar = 1;
+    let itemstoreNumber = 1;
+    let itemscontent = '111';
+    let itemsweight = '111';
+    let itemsdrive = '111';
+    let itemsfrequency = '111';
+    let itemsSensitivity = '111';
+    let itemsconnect = '111';
+    let itemsmains = '111';
+    let itemsEndurance = '111';
+    let itemswatertight = '111';
+    let itemsfeature = '111';
+    let _files=['cgdkddkdpv.jpeg','coknhlkgdv.png','coknhlkocv.jpeg'];
     // 有資料過來就改成 req.body.XXXXXXXXX;
-    let itemName = req.body.itemName;
-    let itemImg = req.body.itemImg;
-    let colorid = req.body.colorid;
-    let itemsbrand = req.body.itemsbrand;
-    let itemstype = req.body.itemstype;
-    let itemPrice = req.body.itemPrice;
-    let itemQty = req.body.itemQty;
-    let itemsales = req.body.itemsales;
-    let itemsstar = req.body.itemsstar;
-    let itemstoreNumber = req.body.itemstoreNumber;
-    let itemscontent = req.body.itemscontent;
-    let itemsweight = req.body.itemsweight;
-    let itemsdrive = req.body.itemsdrive;
-    let itemsfrequency = req.body.itemsfrequency;
-    let itemsSensitivity = req.body.itemsSensitivity;
-    let itemsconnect = req.body.itemsconnect;
-    let itemsmains = req.body.itemsmains;
-    let itemsEndurance = req.body.itemsEndurance;
-    let itemswatertight = req.body.itemswatertight;
-    let itemsfeature = req.body.itemsfeature;
+    // let itemName = req.body.itemName;
+    // let itemImg = req.body.itemImg;
+    // let colorid = req.body.colorid;
+    // let itemsbrand = req.body.itemsbrand;
+    // let itemstype = req.body.itemstype;
+    // let itemPrice = req.body.itemPrice;
+    // let itemQty = req.body.itemQty;
+    // let itemsales = req.body.itemsales;
+    // let itemsstar = req.body.itemsstar;
+    // let itemstoreNumber = req.body.itemstoreNumber;
+    // let itemscontent = req.body.itemscontent;
+    // let itemsweight = req.body.itemsweight;
+    // let itemsdrive = req.body.itemsdrive;
+    // let itemsfrequency = req.body.itemsfrequency;
+    // let itemsSensitivity = req.body.itemsSensitivity;
+    // let itemsconnect = req.body.itemsconnect;
+    // let itemsmains = req.body.itemsmains;
+    // let itemsEndurance = req.body.itemsEndurance;
+    // let itemswatertight = req.body.itemswatertight;
+    // let itemsfeature = req.body.itemsfeature;
     const output = {
         success: false,
         rows: []
@@ -70,12 +83,87 @@ router.post('/add', upload.none(), (req, res) => {
         itemsconnect, itemsmains, itemsEndurance, itemswatertight, itemsfeature
     ])
         .then(([results]) => {
-            output.results = results;
             output.success = true;
-            console.log(output);
+            
+            doSearchItems(itemName,_files);
+            // output.rows = outputWithId.rows;
+            // console.log('結果 ======> ', outputWithId)
+            // return output;
             // res.json(output);
         })
-    // res.json(req.body);
+
+}
+
+// 查詢商品id
+const doSearchItems = async (_itemName,_files) => {
+    let itemName = _itemName;
+    const output = {
+        success: false,
+        rows: []
+    }
+    let _r = `SELECT LAST_INSERT_ID();`;
+    const [r1] = await db.query(_r);
+    if (r1) output.rows = r1;
+    console.log('last ====> ', output.rows[0]['LAST_INSERT_ID()']);
+    doAddImgsOnDatabase(output.rows[0]['LAST_INSERT_ID()'],_files);
+    
+    // return output_2;
+}
+
+// 新增資料庫圖片
+const doAddImgsOnDatabase = async (_id,_files) => {
+    const output = {
+        success: false,
+        rows: []
+    }
+    let itemId = _id;
+    // 列出files所有上傳檔名
+    // for(let i=0;i<req.files.length;i++){
+    //     console.log('req.file = ', req.files[i].originalname);
+    // }
+
+    let multiple_Image, multiple_Image01, multiple_Image02, multiple_Image03, multiple_Image04;
+    let sql = "INSERT INTO multiple_images (`itemId`,`multiple_Image`, `multiple_Image01`, `multiple_Image02`, `multiple_Image03`, `multiple_Image04`) VALUES (?,?,?,?,?,?)";
+    for (let i = 0; i < 5; i++) {
+        if (i < _files.length) {
+            if (i == 0) multiple_Image = _files[i];
+            if (i == 1) multiple_Image01 = _files[i];
+            if (i == 2) multiple_Image02 = _files[i];
+            if (i == 3) multiple_Image03 = _files[i];
+            if (i == 4) multiple_Image04 = _files[i];
+        }
+        else {
+            if (i == 1) multiple_Image01 = '';
+            if (i == 2) multiple_Image02 = '';
+            if (i == 3) multiple_Image03 = '';
+            if (i == 4) multiple_Image04 = '';
+        }
+    }
+    db.query(sql, [itemId, multiple_Image, multiple_Image01, multiple_Image02, multiple_Image03, multiple_Image04])
+        .then(([r]) => {
+            output.rows = r;
+            output.success = true;
+            console.log('output3 =========> ',output);
+            // return output;
+            // res.json(output.rows);
+        })
+
+}
+
+//================================================== root ==============================================================
+// http://localhost:3009/superseller_plus/
+router.get('/', (req, res) => {
+    res.send('superseller_plus root 測試ok');
+});
+
+//================================================== 新增 ==============================================================
+// (測試ok)
+// 新增產品
+// http://localhost:3009/superseller_plus/add
+router.post('/add', async (req, res) => {
+    const output = await doAddItems(req);
+    
+    res.json(output);
 })
 
 //================================================== 圖片上傳 ==============================================================
@@ -85,40 +173,7 @@ router.post('/add', upload.none(), (req, res) => {
 router.post('/try-upload/', upload.array('avatar'), async (req, res) => {
     console.log('========== react(post) superseller_plus -> 上傳檔案 ==========')
     // let point = 0;
-    const output = {
-        success: false,
-        rows: []
-    }
-    let itemId = 9999;
-    // 列出files所有上傳檔名
-    // for(let i=0;i<req.files.length;i++){
-    //     console.log('req.file = ', req.files[i].originalname);
-    // }
-
-    let multiple_Image, multiple_Image01, multiple_Image02, multiple_Image03, multiple_Image04;
-    let sql = "INSERT INTO multiple_images (`itemId`,`multiple_Image`, `multiple_Image01`, `multiple_Image02`, `multiple_Image03`, `multiple_Image04`) VALUES (?,?,?,?,?,?)";
-    for (let i = 0; i < 5; i++) {
-        if (i < req.files.length) {
-            if (i == 0) multiple_Image = req.files[i].originalname;
-            if (i == 1) multiple_Image01 = req.files[i].originalname;
-            if (i == 2) multiple_Image02 = req.files[i].originalname;
-            if (i == 3) multiple_Image03 = req.files[i].originalname;
-            if (i == 4) multiple_Image04 = req.files[i].originalname;
-        }
-        else {
-            if (i == 1) multiple_Image01 = '';
-            if (i == 2) multiple_Image02 = '';
-            if (i == 3) multiple_Image03 = '';
-            if (i == 4) multiple_Image04 = '';
-        }
-    }
-    db.query(sql, [itemId,multiple_Image, multiple_Image01, multiple_Image02, multiple_Image03, multiple_Image04])
-        .then(([r]) => {
-            output.rows = r;
-            output.success = true;
-            console.log(output.rows);
-            res.json(output.rows);
-        })
+    
 })
 
 //================================================== 測試區 ==============================================================
